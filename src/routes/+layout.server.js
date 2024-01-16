30cf20cb88008ffec00b160b6347a73207bf40fe
eyuuus <adiflorko@gmail.com>
2024-01-16 21:59:49 +0100

    yes

--- src/routes/+layout.server.js
+++
@@ -1,5 +0,0 @@
-export const load = () => {
-	return {
-		apiKey: 'd7fd7a2eed9b4078c2476bb88c6cba7f'
-	};
-};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let data;
+	console.log(data);
+	const kToc = (temp) => {
+		return Math.round((temp = temp - 273.1));
+	};
+</script>
+
+<main>w sanoku jest {kToc(data.weatherData.main.temp)}</main>
