b24908776be4be05430e84a2a9bd09bffbec3524
eyuuus <adiflorko@gmail.com>
2024-01-16 09:26:11 +0100

    yes

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,5 @@
+export const load = () => {
+	return {
+		apiKey: 'd7fd7a2eed9b4078c2476bb88c6cba7f'
+	};
+};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let data;
+
+	let text = getyes();
+	async function getyes() {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=sanok&appid=${data.apiKey}`
+		);
+		const promise = await response.json();
+
+		return {
+			promise
+		};
+	}
+	const kToC = (kelvin) => {
+		kelvin += 273.15;
+		return {
+			kelvin
+		};
+	};
+</script>
+
+<main>
+	{#await text}
+		czekaj
+	{:then weather}
+		{weather}
+	{/await}
+</main>
