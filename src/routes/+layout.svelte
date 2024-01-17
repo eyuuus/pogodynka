75478b6de5b9a5e2d275e7dc900d7d9df63251ff
eyuuus <adiflorko@gmail.com>
2024-01-17 21:31:21 +0100

    dawdbawjdh

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<nav>
+	<div id="logo">
+		<img src="favicon.png" alt="" />Weather
+	</div>
+</nav>
+<slot />
+
+<style>
+	nav {
+		background-color: #1f1f1f;
+		width: 100%;
+		height: 9%;
+		display: flex;
+		align-items: center;
+	}
+	#logo {
+		margin-left: 3rem;
+		font-weight: 700;
+		font-size: 1.5rem;
+		color: white;
+	}
+	img {
+		height: 2rem;
+		width: 2rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,42 @@
 	};
 </script>
 
-<main>w sanoku jest {kToc(data.weatherData.main.temp)}</main>
+<main>
+	<div id="banner">
+		<div id="search">
+			<form action="search">
+				<div id="icon"></div>
+				<input type="text" name="city" id="city" placeholder="Search your city" />
+			</form>
+		</div>
+	</div>
+</main>
+
+<style>
+	#banner {
+		width: 100%;
+		height: 78vh;
+		background-image: url($lib/images/banner.jpg);
+		background-size: cover;
+		background-repeat: no-repeat;
+		display: flex;
+		justify-content: center;
+	}
+	#search {
+		width: 30%;
+		height: 3rem;
+	}
+	input {
+		width: 85%;
+		height: 100%;
+	}
+	#icon {
+		width: 15%;
+		height: 100%;
+	}
+	form {
+		height: 100%;
+		display: flex;
+		width: 100%;
+	}
+</style>
