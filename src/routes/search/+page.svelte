50971b2fb2d14fe2011dfcb2f6b690f2522d30f0
eyuuus <adiflorko@gmail.com>
2024-01-24 20:03:52 +0100

    aqwertyuio

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,13 @@
 <script>
 	export let data;
+	import clouds from '$lib/images/cloudy-icon.svg';
+	import cloudsLightning from '$lib/images/cloud-lightning-icon.svg';
+	import cloudsRain from '$lib/images/cloud-rain-icon.svg';
+	import cloudsRainLightning from '$lib/images/cloud-rain-icon.svg';
+	import cloudsSnow from '$lib/images/cloud-snow-icon.svg';
+	import cooling from '$lib/images/cooling-icon.svg';
+	import sunny from '$lib/images/day-sunny-icon.svg';
+	const setImage = () => {};
 	console.log(data);
 	let date = new Date();
 	const kToc = (temp) => {
@@ -13,6 +21,7 @@
 		<p>{data.weatherData}</p>
 		<div class="flex w-3/4 m-auto mt-4">
 			<div class="text-6xl">
+				<img src={data.weatherData.current.weather[0].main == 'Clouds' ? '' : ''} alt="" />
 				{kToc(data.weatherData.current.temp)}&deg;<span class="text-3xl">C</span>
 			</div>
 			<div>
